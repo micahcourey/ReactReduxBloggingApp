03e5efa43621b2efd2eb781022197174c737393f
micahcourey <micahjcourey@gmail.com>
2016-04-20 21:29:06 -0700

    Add links to each posts on the index

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import NewPost from './components/new_post';
+import PostsShow from './components/posts_show';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={NewPost} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
