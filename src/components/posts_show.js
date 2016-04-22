1cc74976932c4b141e6a1fe8641c580ed5e2e12f
micahcourey <micahjcourey@gmail.com>
2016-04-21 20:33:36 -0700

    Add an action creater to fetch the individual post and then display it to the user

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render() {
-    return <div>Show post {this.props.params.id}</div>;
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6><br />
+        <p>{post.content}</p>
+      </div>
+    )
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
